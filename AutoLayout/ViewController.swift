53e44776b56cbefc4449173d29b3f09515184c58
Usuário Convidado <Guest@pa06micro16.local>
2017-04-03 19:14:07 -0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Usuário Convidado on 03/04/17.
+//  Copyright © 2017 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
